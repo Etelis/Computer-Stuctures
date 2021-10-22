e964a3dbe75247ef2dd1f74a8237c175a50dcf85
Itay <etelis2019@gmail.com>
2021-10-22 23:13:52 +0300

    Added statments to functions.

--- ex1.c
+++ ex1.c
@@ -28,14 +28,22 @@ unsigned long long merge_bytes(unsigned long long x, unsigned long long y){
     return (maskY & y) | (maskX & x);
 }
 
+/*
+ * The following function recieves a number and a char and places that char in the offset calculated.
+ * @param x - the number.
+ * @param b - the byte we will replace.
+ * @param i - the offset.
+ */
 unsigned long long put_byte(unsigned long long x, unsigned char b, int i){
     // x = 00010010 00110100 01010110 01111000 11001101 11101111 00110100 01010110
     // b = 10101011 - AB - 1011
     // y = 00000000 00000000 00000000 00000000 00000000 00000000 00000000 10101011
     size_t offset = (sizeof (x) - (1 + i));
+    // if machine is big endian calculate from back to front.
     if (!is_big_endian()){
         offset = sizeof (x) - offset;
     }
+    // otherwise calculate regulary.
     unsigned char *jump = (unsigned char*) &(x);
     jump += offset;
     *jump = b;
